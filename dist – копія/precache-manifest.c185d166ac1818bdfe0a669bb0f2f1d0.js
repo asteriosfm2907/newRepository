dae41330a180b3ee6ea8aa47b069224eceeb3619
asteriosfm2907 <72600853+asteriosfm2907@users.noreply.github.com>
2020-10-09 20:55:05 +0300

    thirdcommit

---
+++ dist – копія/precache-manifest.c185d166ac1818bdfe0a669bb0f2f1d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb90bc342f1badbb312a",
+    "url": "/css/app.92dfbd77.css"
+  },
+  {
+    "revision": "91c0784cecdbcb3badf85ba842de5f21",
+    "url": "/fonts/Montserrat-Medium.91c0784c.ttf"
+  },
+  {
+    "revision": "1bd837780282c6484492d86e1cf0d6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb90bc342f1badbb312a",
+    "url": "/js/app.f5896b3e.js"
+  },
+  {
+    "revision": "6890aeec084d10df532f",
+    "url": "/js/chunk-vendors.c08a6895.js"
+  },
+  {
+    "revision": "cac439e5627fd04e389de3e7dd18fecb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
